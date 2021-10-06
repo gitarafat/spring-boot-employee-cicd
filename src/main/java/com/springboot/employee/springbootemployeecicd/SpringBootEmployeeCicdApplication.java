9c274f182055a793a51a72b5939abdcbc3b9a8ad
ArafatGitSpace <sfbena2016@gmail.com>
2021-10-06 22:05:29 +0530

    my spring boot application cicd

---
+++ src/main/java/com/springboot/employee/springbootemployeecicd/SpringBootEmployeeCicdApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.employee.springbootemployeecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/employee/springbootemployeecicd/SpringBootEmployeeCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.employee.springbootemployeecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmployeeCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
