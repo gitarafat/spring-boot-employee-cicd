96ee3cf270e12e77b8451ad1f8148fd5adc367c5
ArafatGitSpace <sfbena2016@gmail.com>
2021-10-06 22:27:42 +0530

    added a new controller class

---
+++ src/main/java/com/springboot/employee/springbootemployeecicd/controller/EmployeeController.java
@@ -0,0 +1,20 @@
+package com.springboot.employee.springbootemployeecicd.controller;
+
+import com.springboot.employee.springbootemployeecicd.enitiry.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/getAllEmployees")
+    public List<Employee> getAllEmployees(){
+
+        return Arrays.asList(new Employee("Mohamed", "78348"), new Employee("Yasir", "90900"));
+    }
+}
---
+++ src/main/java/com/springboot/employee/springbootemployeecicd/enitiry/Employee.java
@@ -0,0 +1,12 @@
+package com.springboot.employee.springbootemployeecicd.enitiry;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    private String name;
+    private String empno;
+}
